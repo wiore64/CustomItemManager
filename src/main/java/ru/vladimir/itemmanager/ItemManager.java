f58418ca1ff0508dbd4dffef5a0cbd2453278916
Vladimir <vladimirkosilov361@gmail.com>
2026-06-21 12:51:50 +0400

    Project structure is prepared.

---
+++ src/main/java/ru/vladimir/itemmanager/ItemManager.java
@@ -0,0 +1,16 @@
+package ru.vladimir.itemmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ItemManager extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
